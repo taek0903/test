87d523232971fa15c21b77ec6bad08eb805b0593
taek0903 <ico550001@gmail.com>
2026-01-11 14:39:12 +0900

    python_ch05_for1

---
+++ ch05/for1.py
@@ -0,0 +1,30 @@
+# for1.py
+
+# for 변수 in 리스트:
+#     코드블록
+
+for num in [0,1,2]:
+    print("hi")
+    print(num)
+print("-------------------------------")
+
+characters = ['엘리스', '도도새', '3월토끼']
+for character in characters:
+    print(character)
+print("-------------------------------")
+
+for letter in '체셔고양이':
+    print(letter)
+print("-------------------------------")
+
+nums = [0,1,2]
+for num in nums:
+    print(num)
+print(nums)
+print("-------------------------------")
+
+nums = [0,1,2]
+for num in nums:
+    print(num)
+    print(nums)
+print("-------------------------------")
