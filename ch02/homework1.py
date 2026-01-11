b397b9339847bc3ae06c5e44f0884f836432bb25
taek0903 <ico550001@gmail.com>
2026-01-11 14:27:36 +0900

    python_ch02_hw_ch02

---
+++ ch02/homework1.py
@@ -0,0 +1,27 @@
+print("Mary's cosmetics")
+print('신씨가 소리질렀다. "도둑이야"')
+print("안녕하세요.\n만나서 \t\t반갑습니다.")
+print("first", end="") ; print("second")
+
+samsung = 50000
+samsung = int(samsung)
+total = samsung * 10
+print("총 평가금액: ", total)
+
+str1 = 15.79
+str1 = float(str1)
+print(type(str1))
+
+year = 2025
+year = float(year)
+print(type(year))
+print(year+3)
+
+air = 48584
+air = float(air)
+total1 = air * 36
+print(total1,"원")
+
+old = input("나이를 입력하세요: ")
+old=float(old)
+print(old+3)
