3b0219ad91012c03f87546fe244984f223188e90
taek0903 <ico550001@gmail.com>
2026-01-11 15:45:24 +0900

    python_practice_swap

---
+++ practice/swap.py
@@ -0,0 +1,30 @@
+# def count_down(n):
+#     if n == 0:
+#         print("완료!")
+#         return
+#     print(n)
+#     count_down(n-1)
+
+# count_down(5)
+
+# def funca(pa, pb):
+#     temp=pa
+#     pa=pb
+#     pb=temp
+
+# na=10
+# nb=11
+# print('na 값은', na, 'nb 값은', nb)
+# funca(na,nb)
+# print('na 값은', na, 'nb 값은', nb)
+a=6
+if a > 4:
+    print('두')
+if a > 5:
+    print('산')
+if a == 6 :
+    print('로')
+if type(a)==int or type(a)==str:
+    print('키')
+else :
+    print('부트캠프')
