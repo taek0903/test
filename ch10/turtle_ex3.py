0a86c2a6c7919e0e2a220ac9399eced3cd0b5732
taek0903 <ico550001@gmail.com>
2026-01-11 15:14:39 +0900

    python_ch10_turtle_ex1

---
+++ ch10/turtle_ex3.py
@@ -0,0 +1,12 @@
+# turtle.ex3.py
+
+# import turtle
+from turtle import shape
+from turtle import forward
+from turtle import left
+
+shape("turtle")
+
+for x in range(1, 100,1):
+    forward(x)
+    left(90)
