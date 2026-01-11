05b2e3a4c496e26e183bf5c6451061c6ee6785f9
taek0903 <ico550001@gmail.com>
2026-01-11 14:32:59 +0900

    python_ch02_print_function

---
+++ ch02/print_func.py
@@ -0,0 +1,15 @@
+#print_func.py
+
+na = 10
+nb = 20.2
+sa = "python"
+print("na변수값", na, sep="")
+print("nb변수값", nb)
+print("sa변수값", sa)
+
+print("----------------------------")
+nc = 30  # 초기화
+nd = 40
+print("nc=", nc, "nd=", nd)
+nd = nc  # 재할당
+print("nc=", nc, "nd=", nd)
