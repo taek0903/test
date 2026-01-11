af9cabea96243120dc6022ae66ad3da895480b14
taek0903 <ico550001@gmail.com>
2026-01-11 14:33:45 +0900

    python_ch02_var

---
+++ ch02/var1.py
@@ -0,0 +1,49 @@
+
+
+rainbow = "빨주노초파남보"
+print(rainbow)
+
+count = 0
+print(count)
+
+count = 1
+print(count)
+
+count = count + 1
+print(count)
+
+na = 10
+nb = 20.2
+print(na)
+print(nb)
+print(type(na))
+print(type(nb))
+
+# print(na, nb)
+
+
+sa = "python"
+print(sa)
+print(type(sa))
+
+print(na, nb, sa)
+
+# 정의되지 않은 변수 사용 불가
+# print(numbers1)
+
+sa = input()
+print("입력된 값은 = ")
+print(sa)
+
+na = 10
+nb = 20.2
+sa = "python"
+print("na변수값", na, sep="")
+print("nb변수값", nb)
+print("sa변수값", sa)
+
+nc = 30
+nd = 40
+print("nc=", nc, "nd=", nd)
+nd = nc
+print("nc=", nc, "nd=", nd)
