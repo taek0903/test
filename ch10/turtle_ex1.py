d57777872c67fc2d34c18da172b06dbf5d214653
taek0903 <ico550001@gmail.com>
2026-01-11 15:14:30 +0900

    python_ch10_turtle_ex1

---
+++ ch10/turtle_ex1.py
@@ -0,0 +1,15 @@
+# turtle.ex1.py
+
+import turtle
+
+# turtle.shape("turtle")
+
+p = turtle.Pen()
+p.shape('turtle')
+
+for x in range(1, 100,1):
+    # turtle.forward(x)
+    # turtle.left(90)
+    p.forward(x)
+    p.left(90)
+
