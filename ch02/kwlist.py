582ec34aa586864ddcb0c9f146b7861a2a588d64
taek0903 <ico550001@gmail.com>
2026-01-11 14:29:44 +0900

    python_ch02_keyword_list

---
+++ ch02/kwlist.py
@@ -0,0 +1,19 @@
+# kwlist.py
+
+import keyword
+print(keyword.kwlist)
+
+my_int = 3
+print(my_int)
+
+# 3rd_num = 3
+# print(3rd_num)
+
+# my string = "abc"
+# print(my string)
+
+# if = "abcd"
+# print(if)
+
+na = 10
+print(na)
